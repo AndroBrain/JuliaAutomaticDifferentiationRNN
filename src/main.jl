eb12aaf6456f4963ce8899f67c23624783c0a083
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-06-02 12:22:45 +0200

    Add epochs handling

--- src/main.jl
+++ src/main.jl
@@ -38,10 +38,11 @@ end
 function main()
     # (in) => (out)
     rnn = LayerWrapper(RecurrentNetworkModule.RNN(784 => 64, tanh), Descent(1f-10))
-    dense1 = LayerWrapper(DenseNetworkModule.Dense(784 => 64, identity), Descent(1f-10))
-    dense2 = LayerWrapper(DenseNetworkModule.Dense(64 => 10, identity), Descent(0.5f0))
+    dense1 = LayerWrapper(DenseNetworkModule.Dense(784 => 64, identity), Descent(0.00000000002f0))
+    dense2 = LayerWrapper(DenseNetworkModule.Dense(64 => 10, identity), Descent(1f0))
     model = [rnn, dense2]
     batch_size = 100
+    epochs = 5
 
     println("Loading training data...")
     train_x, train_y = DataModule.preprocess(:train; one_hot = true)
@@ -54,18 +55,21 @@ function main()
 
     println("Training...")
     batch_acc = Float64[]
-    batches = size(train_x_batched, 1)
-    @time begin
-        for batch in 1:batches
-            result = train(model, train_x_batched[batch], train_y_batched[batch])
-            loss, acc, _ = AccuracyModule.loss_and_accuracy(result, train_y_batched[batch])
-            push!(batch_acc, acc)
+    for epoch in 1:epochs
+        batches = size(train_x_batched, 1)
+        @time begin
+            for batch in 1:batches
+                result = train(model, train_x_batched[batch], train_y_batched[batch])
+                loss, acc, _ = AccuracyModule.loss_and_accuracy(result, train_y_batched[batch])
+                push!(batch_acc, acc)
+            end
         end
+        result = forward(model, train_x)
+        _, train_acc, _ = AccuracyModule.loss_and_accuracy(result, train_y)
+        result = forward(model, test_x)
+        _, test_acc, _ = AccuracyModule.loss_and_accuracy(result, test_y)
+        @info epoch train_acc test_acc
     end
-    result = forward(model, train_x)
-    loss, acc, _ = AccuracyModule.loss_and_accuracy(result, train_y)
-    println("Trained")
-    @show acc
 
     plot(batch_acc, xlabel="Batch num", ylabel="acc", title="Accuracy over batches")
 end
