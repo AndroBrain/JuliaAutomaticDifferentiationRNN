90a657d3b4f9850ba0a7f4912526a53d35315ee8
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-06-03 17:49:14 +0200

    Make it easier to test learning_rates

--- src/GradientOptimizersModule.jl
+++ src/GradientOptimizersModule.jl
@@ -1,5 +1,5 @@
 module GradientOptimizersModule
-    export GradientOptimizer, Adagrad, Descent
+    export GradientOptimizer, Adagrad, Descent, RMSProp, Adam
 
     abstract type GradientOptimizer end
 
@@ -14,6 +14,18 @@ module GradientOptimizersModule
         return delta
     end
 
+    mutable struct RMSProp <: GradientOptimizer
+       gradient_squared
+       decay_rate
+       learning_rate
+    end
+
+    function (r::RMSProp)(g)
+       r.gradient_squared = r.gradient_squared .* r.decay_rate + g.^2 .* (1 - r.decay_rate)
+       delta = r.learning_rate .* g ./ (sqrt.(r.gradient_squared) .+ 10e-12)
+        return delta
+    end
+
     struct Descent <: GradientOptimizer
         learning_rate
     end
@@ -22,18 +34,18 @@ module GradientOptimizersModule
         return d.learning_rate .* g
     end
 
-    # WORK IN PROGRESS
-    struct Adam <: GradientOptimizer
-        momentum
-        momentum_rate
-        velocity
-        velocity_rate
+    mutable struct Adam <: GradientOptimizer
+        gradient
+        gradient_squared
+        beta1
+        beta2
+        learning_rate
     end
 
     function (a::Adam)(g)
-        momentum = a.momentum_rate .* a.momentum + (1 - a.momentum_rate) .* g
-        velocity = a.velocity_rate * a.velocity + (1 - a.velocity_rate) .* (g.^2)
-        a.momentum = momentum
-        a.velocity = velocity
+        a.gradient = a.gradient .* a.beta1 + g .* (1 - a.beta1)
+        a.gradient_squared = a.gradient_squared * a.beta2 + g.^2 .* (1 - a.beta2)
+        delta = a.learning_rate .* a.gradient ./ (sqrt.(a.gradient_squared) .+ 10e-12)
+        return delta
     end
 end
--- src/main.jl
+++ src/main.jl
@@ -31,29 +31,13 @@ function forward(model, input::Matrix{Float32})
     return input
 end
 
-function main()
-    batch_size = 100
-    epochs = 5
-    # (in) => (out)
-    rnn = LayerWrapper(RecurrentNetworkModule.RNN(196 => 64, tanh), Adagrad(zeros(Float32, 64, batch_size), 10e-8))
-    dense = LayerWrapper(DenseNetworkModule.Dense(64 => 10, identity), Descent(10e-1))
-    model = [rnn, dense]
-
-    println("Loading training data...")
-    train_x, train_y = DataModule.preprocess(:train; one_hot = true)
-
-    train_x_batched = DataModule.batch(train_x, batch_size)
-    train_y_batched = DataModule.batch(train_y, batch_size)
-
-    println("Loading test data...")
-    test_x, test_y = DataModule.preprocess(:test; one_hot = true)
-
-    println("Training...")
+function train(model, epochs, train_x_batched, train_y_batched, train_x, train_y, test_x, test_y)
     batch_acc = Float64[]
     batch_loss = Float64[]
+    final_acc = 0
     for epoch in 1:epochs
         batches = size(train_x_batched, 1)
-        @time begin
+#         @time begin
             for batch in 1:batches
                 # TODO go back only once like in the notebook example:
                 # https://gist.github.com/bchaber/48a309fbdad8753d2a60ce2f30da5e44
@@ -74,7 +58,7 @@ function main()
                 push!(batch_acc, acc)
                 push!(batch_loss, loss)
             end
-        end
+#         end
         for layer in model
             if isa(layer.layer, RecurrentNetworkModule.RNNCell)
                 layer.layer.state = layer.layer.state0
@@ -95,14 +79,58 @@ function main()
         forward(model, test_x[393:588,:])
         result = forward(model, test_x[589:end,:])
         _, test_acc, _ = AccuracyModule.loss_and_accuracy(result, test_y)
-        @info epoch train_acc test_acc
+        final_acc = test_acc
+#         @info epoch train_acc test_acc
         for layer in model
             if isa(layer.layer, RecurrentNetworkModule.RNNCell)
                 layer.layer.state = layer.layer.state0
             end
         end
     end
-
+    return final_acc
 #     plot(batch_loss, xlabel="Batch num", ylabel="loss", title="Loss over batches")
-    plot(batch_acc, xlabel="Batch num", ylabel="acc", title="Accuracy over batches")
+#     plot(batch_acc, xlabel="Batch num", ylabel="acc", title="Accuracy over batches")
+end
+
+function main()
+    batch_size = 100
+    println("Loading training data...")
+    train_x, train_y = DataModule.preprocess(:train; one_hot = true)
+
+    train_x_batched = DataModule.batch(train_x, batch_size)
+    train_y_batched = DataModule.batch(train_y, batch_size)
+
+    println("Loading test data...")
+    test_x, test_y = DataModule.preprocess(:test; one_hot = true)
+
+    println("Training...")
+    learning_rates = [0.005, 10e-2, 0.0005, 10e-3, 0.00005, 10e-4, 10e-5, 10e-6, 10e-7, 10e-8, 10e-9, 10e-10]
+    small_rates = [10e-1, 0.5, 0.075, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+    best_acc = 0
+    best_small_rate = 0
+    best_rate = 0
+    for small_rate in small_rates
+        for learning_rate in learning_rates
+            @show "In progress:", small_rate, " : ", learning_rate
+            epochs = 5
+            # (in) => (out)
+            rnn = LayerWrapper(RecurrentNetworkModule.RNN(196 => 64, tanh), Adam(zeros(Float32, 64, batch_size), zeros(Float32, 64, batch_size), 0.9, 0.999, learning_rate))
+    #         rnn = LayerWrapper(RecurrentNetworkModule.RNN(196 => 64, tanh), Descent(1.0))
+            dense = LayerWrapper(DenseNetworkModule.Dense(64 => 10, identity), Adam(zeros(Float32, 10, batch_size), zeros(Float32, 10, batch_size), 0.9, 0.999, small_rate))
+
+            model = [rnn, dense]
+            acc = train(model, epochs, train_x_batched, train_y_batched, train_x, train_y, test_x, test_y)
+            if acc > best_acc
+                @show acc
+                @show learning_rate
+                @show best_small_rate
+                best_acc = acc
+                best_rate = learning_rate
+                best_small_rate = small_rate
+            end
+        end
+    end
+    @show best_acc
+    @show best_rate
+    @show best_small_rate
 end
