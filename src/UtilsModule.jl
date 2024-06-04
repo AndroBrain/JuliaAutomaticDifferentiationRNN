90af5c51342268620956f8fbceb2130b37b09d72
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-06-04 13:52:55 +0200

    A bit of code cleanup

--- src/UtilsModule.jl
+++ src/UtilsModule.jl
@@ -15,6 +15,10 @@ module UtilsModule
       (rand(Float32, dims...) .- 0.5f0) .* scale
     end
 
+    function zeros32(dims)
+        return zeros(Float32, dims)
+    end
+
     function _match_eltype(layer, ::Type{T}, x::AbstractArray{<:Union{AbstractFloat, Integer}}) where {T}
       convert(AbstractArray{T}, x)
     end
@@ -35,11 +39,4 @@ module UtilsModule
     function create_bias(weights::AbstractArray, bias::Bool, dims::Integer...)
       bias ? fill!(similar(weights, dims...), 0) : false
     end
-
-    function create_bias(weights::AbstractArray, bias::AbstractArray, dims::Integer...)
-      size(bias) == dims || throw(DimensionMismatch("expected bias of size $(dims), got size $(size(bias))"))
-      convert(AbstractArray{_eltype(weights)}, bias)
-    end
-
-    fast_act(f::F, ::AbstractArray = 1:0) where {F<:Function} = f
 end
--- src/main.jl
+++ src/main.jl
@@ -1,101 +1,20 @@
 include("DataModule.jl")
-include("RecurrentNetworkModule.jl")
-include("DenseNetworkModule.jl")
+# include("RecurrentNetworkModule.jl")
+# include("DenseNetworkModule.jl")
 include("AccuracyModule.jl")
 include("GradientOptimizersModule.jl")
+include("ModelModule.jl")
 
-using .DataModule, .RecurrentNetworkModule, .DenseNetworkModule, .AccuracyModule, .GradientOptimizersModule
-using Plots, Random
+using .DataModule, .AccuracyModule, .GradientOptimizersModule, .ModelModule
+using Plots, Random, Statistics
 
 struct LayerWrapper
     layer
     optimizer::GradientOptimizer
 end
 
-function backward(model, C)
-    for layer in reverse(model)
-        C = layer.optimizer(C)
-        if isa(layer.layer, DenseNetworkModule.Dense)
-            C = DenseNetworkModule.back(layer.layer, C)
-        end
-        if isa(layer.layer, RecurrentNetworkModule.RNNCell)
-            C = RecurrentNetworkModule.back(layer.layer, C)
-        end
-    end
-end
-
-function forward(model, input::Matrix{Float32})
-    for layer in model
-        input = layer.layer(input)
-    end
-    return input
-end
-
-function train(model, epochs, train_x_batched, train_y_batched, train_x, train_y, test_x, test_y)
-    batch_acc = Float64[]
-    batch_loss = Float64[]
-    final_acc = 0
-    for epoch in 1:epochs
-        batches = size(train_x_batched, 1)
-        @time begin
-            for batch in randperm(batches)
-                for layer in model
-                    if isa(layer.layer, RecurrentNetworkModule.RNNCell)
-                        layer.layer.state = layer.layer.state0
-                    end
-                end
-                x = train_x_batched[batch][1:196, :]
-                forward(model, x)
-                x = train_x_batched[batch][197:392, :]
-                forward(model, x)
-                x = train_x_batched[batch][393:588, :]
-                forward(model, x)
-                x = train_x_batched[batch][589:end, :]
-                result4 = forward(model, x)
-
-                y = train_y_batched[batch]
-                loss, acc, C = AccuracyModule.loss_and_accuracy(result4, y)
-                C = C ./ 4 # 4 because we divide data into 4 mini-batches
-                backward(model, C)
-                push!(batch_acc, acc)
-                push!(batch_loss, loss)
-            end
-        end
-        for layer in model
-            if isa(layer.layer, RecurrentNetworkModule.RNNCell)
-                layer.layer.state = layer.layer.state0
-            end
-        end
-        forward(model, train_x[1:196,:])
-        forward(model, train_x[197:392,:])
-        forward(model, train_x[393:588,:])
-        result = forward(model, train_x[589:end,:])
-        train_loss, train_acc, _ = AccuracyModule.loss_and_accuracy(result, train_y)
-        for layer in model
-            if isa(layer.layer, RecurrentNetworkModule.RNNCell)
-                layer.layer.state = layer.layer.state0
-            end
-        end
-        forward(model, test_x[1:196,:])
-        forward(model, test_x[197:392,:])
-        forward(model, test_x[393:588,:])
-        result = forward(model, test_x[589:end,:])
-        test_loss, test_acc, _ = AccuracyModule.loss_and_accuracy(result, test_y)
-        final_acc = test_acc
-        @info epoch train_acc train_loss test_acc test_loss
-        for layer in model
-            if isa(layer.layer, RecurrentNetworkModule.RNNCell)
-                layer.layer.state = layer.layer.state0
-            end
-        end
-    end
-    plot(batch_loss, xlabel="Batch num", ylabel="loss", title="Loss over batches")
-#     plot(batch_acc, xlabel="Batch num", ylabel="acc", title="Accuracy over batches")
-end
-
-function main()
-    batch_size = 100
-    println("Loading training data...")
+function load_data(batch_size)
+    println("Loading train data...")
     train_x, train_y = DataModule.preprocess(:train; one_hot = true)
 
     train_x_batched = DataModule.batch(train_x, batch_size)
@@ -103,13 +22,56 @@ function main()
 
     println("Loading test data...")
     test_x, test_y = DataModule.preprocess(:test; one_hot = true)
+    return train_x, train_y, train_x_batched, train_y_batched, test_x, test_y
+end
+
+function train(model, epochs, mini_batches, train_x_batched, train_y_batched, train_x, train_y, test_x, test_y)
+    batch_acc = Float64[]
+    batch_loss = Float64[]
+    batches = randperm(size(train_x_batched, 1))
+    input_size = size(train_x_batched[1], 1)
+    mini_batch_size = convert(Int64, input_size / mini_batches)
+    for epoch in 1:epochs
+        @time begin
+            for batch in batches
+                zero_state(model)
+
+                result = mini_batch_forward(model, train_x_batched[batch], mini_batches, mini_batch_size)
+
+                y = train_y_batched[batch]
+                loss, acc, C = AccuracyModule.loss_and_accuracy(result, y)
+                C = C ./ mini_batches
+                backward(model, C)
+                push!(batch_acc, acc)
+                push!(batch_loss, loss)
+            end
+        end
+        zero_state(model)
+        result = mini_batch_forward(model, train_x, mini_batches, mini_batch_size)
+        train_loss, train_acc, _ = AccuracyModule.loss_and_accuracy(result, train_y)
+
+        zero_state(model)
+        result = mini_batch_forward(model, test_x, mini_batches, mini_batch_size)
+        test_loss, test_acc, _ = AccuracyModule.loss_and_accuracy(result, test_y)
+
+        @info epoch train_acc train_loss test_acc test_loss
+    end
+    plot(batch_loss, xlabel="Batch num", ylabel="loss", title="Loss over batches")
+#     plot(batch_acc, xlabel="Batch num", ylabel="acc", title="Accuracy over batches")
+end
+
+function main()
+    batch_size = 100
+    mini_batches = 4
+    train_x, train_y, train_x_batched, train_y_batched, test_x, test_y = load_data(batch_size)
 
     println("Training...")
     # (in) => (out)
-    rnn = LayerWrapper(RecurrentNetworkModule.RNN(196 => 64, tanh), Descent(15e-3))
-    dense = LayerWrapper(DenseNetworkModule.Dense(64 => 10, identity), Descent(15e-3))
+    descent = Descent(15e-3)
+    rnn = LayerWrapper(RecurrentNetworkModule.RNN(196 => 64, tanh), descent)
+    dense = LayerWrapper(DenseNetworkModule.Dense(64 => 10, identity), descent)
 
     model = [rnn, dense]
     epochs = 5
-    train(model, epochs, train_x_batched, train_y_batched, train_x, train_y, test_x, test_y)
+    train(model, epochs, mini_batches, train_x_batched, train_y_batched, train_x, train_y, test_x, test_y)
 end
