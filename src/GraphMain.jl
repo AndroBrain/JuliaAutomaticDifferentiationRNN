d890b6987b3fb50d3a5bcdabf7ccf7138097d8c8
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-06-20 15:40:16 +0200

    USe proper backward pass

--- src/GraphMain.jl
+++ src/GraphMain.jl
@@ -38,7 +38,10 @@ function main()
 
     epochs = 5
 
-    x = Variable([0.], name = "x")
+    x1 = Variable([0.], name = "x")
+    x2 = Variable([0.], name = "x")
+    x3 = Variable([0.], name = "x")
+    x4 = Variable([0.], name = "x")
 
     wd = Variable(UtilsModule.glorot_uniform(10, 64))
     bd = Variable(UtilsModule.glorot_uniform(10, ))
@@ -46,20 +49,20 @@ function main()
     dfd = Constant(UtilsModule.identity_deriv)
 
     wr = Variable(UtilsModule.glorot_uniform(64, 196))
-    br = Variable(UtilsModule.glorot_uniform(64, ))
     hwr = Variable(UtilsModule.glorot_uniform(64, 64))
-    states = Variable(nothing, name = "states")
-    xes = Variable(nothing, name = "xes")
+    br = Variable(UtilsModule.glorot_uniform(64, ))
     fr = Constant(tanh)
     dfr = Constant(UtilsModule.tanh_deriv)
-    wr0 = Constant(zeros(Float32, 64, 196))
-    hwr0 = Constant(zeros(Float32, 64, 64))
-    br0 = Constant(zeros(Float32, 64, ))
+
+    state0 = Variable(zeros(64))
 
     optimizer = GradientOptimizersModule.Descent(15e-3)
 
-    r = rnn_layer(x, wr, br, hwr, states, xes, fr, dfr, wr0, hwr0, br0)
-    d = dense_layer(r, wd, bd, fd, dfd)
+    r1 = rnn_layer(wr, hwr, state0, br, x1)
+    r2 = rnn_layer(wr, hwr, r1, br, x2)
+    r3 = rnn_layer(wr, hwr, r2, br, x3)
+    r4 = rnn_layer(wr, hwr, r3, br, x4)
+    d = dense_layer(r4, wd, bd, fd, dfd)
     graph = topological_sort(d)
 
     batch_loss = Float64[]
@@ -67,18 +70,11 @@ function main()
     for epoch in 1:epochs
         batches = randperm(size(train_x_batched, 1))
         @time for batch in batches
-            states.output = nothing
-            xes.output = nothing
-            x.output = train_x_batched[batch][  1:196,:]
-            forward!(graph)
-
-            x.output = train_x_batched[batch][197:392,:]
-            forward!(graph)
-
-            x.output = train_x_batched[batch][393:588,:]
-            forward!(graph)
-
-            x.output = train_x_batched[batch][589:end,:]
+            state0.output = zeros(64, 100)
+            x1.output = train_x_batched[batch][  1:196,:]
+            x2.output = train_x_batched[batch][197:392,:]
+            x3.output = train_x_batched[batch][393:588,:]
+            x4.output = train_x_batched[batch][589:end,:]
             result = forward!(graph)
 
             loss = AccuracyModule.loss(result, train_y_batched[batch])
@@ -87,20 +83,13 @@ function main()
             backward!(graph, seed=gradient)
             update_weights!(graph, optimizer)
         end
-        xes.output = nothing
-        states.output = nothing
+        state0.output = zeros(64, 10000)
         test_graph = topological_sort(d)
 
-        x.output = test_x[  1:196,:]
-        forward!(test_graph)
-
-        x.output = test_x[197:392,:]
-        forward!(test_graph)
-
-        x.output = test_x[393:588,:]
-        forward!(test_graph)
-
-        x.output = test_x[589:end,:]
+        x1.output = test_x[  1:196,:]
+        x2.output = test_x[197:392,:]
+        x3.output = test_x[393:588,:]
+        x4.output = test_x[589:end,:]
         result = forward!(test_graph)
 
         loss = AccuracyModule.loss(result, test_y)
